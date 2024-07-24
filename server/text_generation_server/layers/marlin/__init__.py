5b6ba336edc2b640559d7d2f71ee5117ea40a64d
Daniël de Kok <me@danieldk.eu>
2024-07-24 14:36:52 +0000

    Handle GPTQ-Marlin loading in `GPTQMarlinWeightLoader`
    The `GPTWeightLoader` was structured like this in pseudocode:
    
    if marlin:
      Set up tensors in a way that GPTQ-Marlin expects
    else:
      Set up tensors in a way that ExLlama/GPTQ/AWQ expect
    
    However, the GPT-Marlin implementation details should really be in the
    `marlin` module. So move the former part out to a separate
    `GPTQMarlinWeightsLoader`.

--- server/text_generation_server/layers/marlin/__init__.py
+++ server/text_generation_server/layers/marlin/__init__.py
@@ -1,10 +1,8 @@
 from typing import List, Tuple
 
-import torch
 from text_generation_server.layers.marlin.fp8 import GPTQMarlinFP8Linear
 from text_generation_server.layers.marlin.gptq import (
-    GPTQMarlinLinear,
-    GPTQMarlinWeight,
+    GPTQMarlinWeightsLoader,
     can_use_gptq_marlin,
     repack_gptq_for_marlin,
 )
@@ -12,8 +10,7 @@ from text_generation_server.layers.marlin.marlin import MarlinWeightsLoader
 
 __all__ = [
     "GPTQMarlinFP8Linear",
-    "GPTQMarlinLinear",
-    "GPTQMarlinWeight",
+    "GPTQMarlinWeightsLoader",
     "MarlinWeightsLoader",
     "can_use_gptq_marlin",
     "repack_gptq_for_marlin",
